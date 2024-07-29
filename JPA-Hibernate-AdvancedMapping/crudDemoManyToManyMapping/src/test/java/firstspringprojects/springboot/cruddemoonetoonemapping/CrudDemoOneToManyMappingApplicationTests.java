e7fea62d025447ff5e3e24de2c7a15ed7575ae19
KarchevBorislav <bkarchev@web.de>
2024-07-29 21:46:04 +0200

    ManyToOne

---
+++ JPA-Hibernate-AdvancedMapping/crudDemoManyToManyMapping/src/test/java/firstspringprojects/springboot/cruddemoonetoonemapping/CrudDemoOneToManyMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package firstspringprojects.springboot.cruddemoonetoonemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoOneToManyMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
